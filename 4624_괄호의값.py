f4b8146d9eb71c2378e20adf6512c9797204d4e2
Hayoon <gkdbssla97@gmail.com>
2022-05-21 20:03:59 +0900

    4624_괄호의값

---
+++ 4624_괄호의값.py
@@ -0,0 +1,36 @@
+bracket = list(input())
+
+stack = []
+result = 0
+tmp = 1
+
+for i in range(len(bracket)):
+    if bracket[i] == '(':
+        stack.append(bracket[i])
+        tmp *= 2
+    elif bracket[i] == '[':
+        stack.append(bracket[i])
+        tmp *= 3
+    elif bracket[i] == ')':
+        if not stack or stack[-1] == '[':
+            #print(stack[-1])
+            result = 0
+            break
+        if bracket[i-1] == '(':
+            result += tmp
+        stack.pop()
+        tmp //= 2
+    else:
+        if not stack or stack[-1] == '(':
+            result = 0
+            break
+        if bracket[i-1] == '[':
+            result += tmp
+        stack.pop()
+        tmp //= 3
+    #print(stack[-1])
+
+if stack:
+    print(0)
+else:
+    print(result)
